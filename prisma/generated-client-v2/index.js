32703f97e9e94e76ca335d144b9f1b9135c76a59
Abdirahim Omar Hassan <abdirahimomar147@gmail.com>
2026-02-07 16:06:06 +0300

    update happened

--- prisma/generated-client-v2/index.js
+++ prisma/generated-client-v2/index.js
@@ -241,7 +241,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "..",
@@ -251,7 +251,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
